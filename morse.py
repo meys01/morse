fe1800128dd08109afec5c9b88b347dcca3e40f8
meys01 <37961705+meys01@users.noreply.github.com>
2020-01-21 16:10:59 +0100

    tests OK

--- morse.py
+++ morse.py
@@ -2,4 +2,38 @@
 # pylint: disable=missing-docstring
 
 def decode(message):
-    pass # TODO: implement the behavior!
+    MORSE_CODE_DICT = { '' : '',
+                        '.-' : 'A',
+                        '-...' : 'B',
+                        '-.-.' :'C',
+                        '-..' : 'D',
+                        '.' : 'E',
+                        '..-.' : 'F',
+                        '--.' : 'G',
+                        '....' : 'H',
+                        '..' : 'I',
+                        '.---' : 'J',
+                        '-.-' : 'K',
+                        '.-..' : 'L',
+                        '--' : 'M',
+                        '-.' : 'N',
+                        '---' : 'O',
+                        '.--.' : 'P',
+                        '--.-' : 'Q',
+                        '.-.' : 'R',
+                        '...' : 'S',
+                        '-' : 'T',
+                        '..-' : 'U',
+                        '...-' : 'V',
+                        '.--' : 'W',
+                        '-..-' : 'X',
+                        '-.--' : 'Y',
+                        '--..' : 'Z'
+                        }
+
+    message_split = message.split(' ')
+    result = ""
+
+    for code in range(len(message_split)):
+        result += MORSE_CODE_DICT[message_split[code]]
+    return result
