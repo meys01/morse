e8dd175cd6a73ce6622f2f0ad5a76d0651907e4f
meys01 <37961705+meys01@users.noreply.github.com>
2020-01-21 16:30:40 +0100

    Add sentence decoding

--- morse.py
+++ morse.py
@@ -1,39 +1,44 @@
 # morse.py
 # pylint: disable=missing-docstring
+MORSE_CODE_DICT = { '' : '',
+                    '.-' : 'A',
+                    '-...' : 'B',
+                    '-.-.' :'C',
+                    '-..' : 'D',
+                    '.' : 'E',
+                    '..-.' : 'F',
+                    '--.' : 'G',
+                    '....' : 'H',
+                    '..' : 'I',
+                    '.---' : 'J',
+                    '-.-' : 'K',
+                    '.-..' : 'L',
+                    '--' : 'M',
+                    '-.' : 'N',
+                    '---' : 'O',
+                    '.--.' : 'P',
+                    '--.-' : 'Q',
+                    '.-.' : 'R',
+                    '...' : 'S',
+                    '-' : 'T',
+                    '..-' : 'U',
+                    '...-' : 'V',
+                    '.--' : 'W',
+                    '-..-' : 'X',
+                    '-.--' : 'Y',
+                    '--..' : 'Z'
+                    }
 
 def decode(message):
-    MORSE_CODE_DICT = { '' : '',
-                        '.-' : 'A',
-                        '-...' : 'B',
-                        '-.-.' :'C',
-                        '-..' : 'D',
-                        '.' : 'E',
-                        '..-.' : 'F',
-                        '--.' : 'G',
-                        '....' : 'H',
-                        '..' : 'I',
-                        '.---' : 'J',
-                        '-.-' : 'K',
-                        '.-..' : 'L',
-                        '--' : 'M',
-                        '-.' : 'N',
-                        '---' : 'O',
-                        '.--.' : 'P',
-                        '--.-' : 'Q',
-                        '.-.' : 'R',
-                        '...' : 'S',
-                        '-' : 'T',
-                        '..-' : 'U',
-                        '...-' : 'V',
-                        '.--' : 'W',
-                        '-..-' : 'X',
-                        '-.--' : 'Y',
-                        '--..' : 'Z'
-                        }
+    word = message.split("/")
+    sentence = [decode_word(s) for s in word]
+    return ' '.join(sentence)
 
-    message_split = message.split(' ')
-    result = ""
 
-    for code in range(len(message_split)):
-        result += MORSE_CODE_DICT[message_split[code]]
-    return result
+def decode_word(message):
+    if message == "":
+        return ""
+
+    symbols = message.split(" ")
+    letters = [MORSE_CODE_DICT[s] for s in symbols]
+    return ''.join(letters)
